6f9da955d2f3c0f11b10e12d981f9eea52f68717
Jérémy Choisy <choisy.jeremy@gmail.com>
2020-11-16 09:27:18 +0100

    update server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname,'public')));
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port);
 console.log('server started '+ port);
